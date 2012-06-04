18346f275bc4a3e66be1d9ca34c88d238d67362b
cesine <cesine@yahoo.com>
2012-06-04 11:00:34 -0400

    copying the infinite paging example to test out modifying it to suit our project

---
+++ examples/netflix-infinite-paging-requirejs/views/AppView.js
@@ -0,0 +1,31 @@
+(function ( views ) {
+
+	views.AppView = Backbone.View.extend({
+		el : '#content',
+
+		initialize : function () {
+
+			var tags = this.collection;
+
+			tags.on('add', this.addOne, this);
+			tags.on('reset', this.addAll, this);
+			tags.on('all', this.render, this);
+
+			tags.pager();
+
+		},
+
+		addAll : function () {
+			this.collection.each (this.addOne);
+		},
+		
+		addOne : function ( item ) {
+			var view = new views.ResultView({model:item});
+			$('#content').append(view.render().el);
+		},
+
+		render: function(){
+		}
+	});
+
+})( app.views );
