732c17ba991b54f3a1d43a1ee5f053222b3e865a
Will Sulzer <will@kingofweb.com>
2013-01-30 10:50:45 -0800

    Fixed an issue with bad assertion clientPager_test that caused warnings. Refactored ugly callback handler in clientPager.pager to use a cleaner _.result function.

--- test/backbone.paginator.clientPager_test.js
+++ test/backbone.paginator.clientPager_test.js
@@ -540,8 +540,8 @@ describe('backbone.paginator.clientPager', function() {
       expect(successCallbackSpy.calledOnce).to.equal(true);
     });
     it('should not try to invoke an undefined success callback', function() {
-      expect(this.clientPagerTest.pager()).to.be.undefined;
-      expect(this.clientPagerTest.pager({error: function() {}})).to.undefined;
+      expect(this.clientPagerTest.pager()).to.be.an('undefined');
+      expect(this.clientPagerTest.pager({error: function() {}})).to.an('undefined');
     });
   });
 
