66c6aad8dba337fcf58ea9fbd9df0bb2ad3536aa
Leonard Ehrenfried <leonard.ehrenfried@gmail.com>
2013-03-24 21:25:29 +0100

    Remove superfluous code from netflix example

--- examples/netflix-request-paging/views/AppView.js
+++ examples/netflix-request-paging/views/AppView.js
@@ -8,16 +8,10 @@
       var tags = this.collection;
 
       tags.on('add', this.addOne, this);
-      tags.on('reset', this.addAll, this);
       tags.on('all', this.render, this);
 
       tags.pager();
 
-    },
-
-    addAll : function () {
-      this.$el.empty();
-      this.collection.each (this.addOne);
     },
 
     addOne : function ( item ) {
