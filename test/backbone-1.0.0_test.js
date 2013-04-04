8cd63fb006ef1105528eae32652501cb983aeaa4
Leonard Ehrenfried <leonard.ehrenfried@gmail.com>
2013-04-04 21:56:12 +0200

    Wrap test case in describe function

--- test/backbone-1.0.0_test.js
+++ test/backbone-1.0.0_test.js
@@ -4,39 +4,40 @@
 
 // this file contains tests which target behaviour found in backbone 1.0.0
 
-it('should use "promise-style" `options.success` arguments', function(done){
+describe("Backbone 1.0.0 specific functionality", function() {
+  it('should use "promise-style" `options.success` arguments', function(done){
 
-  // 0.9.10 had changed the arguments but 1.0.0 reverted back to what it used to be
-  // in 0.9.9 and earlier
+    // 0.9.10 had changed the arguments but 1.0.0 reverted back to what it used to be
+    // in 0.9.9 and earlier
 
-  var requestPagerTest = {
-    paginator_ui: {},
-    paginator_core: {
-      type: 'GET',
-      dataType: 'json'
-    }
-  };
-  _.extend(requestPagerTest, new Backbone.Paginator.requestPager());
+    var requestPagerTest = {
+      paginator_ui: {},
+      paginator_core: {
+        type: 'GET',
+        dataType: 'json'
+      }
+    };
+    _.extend(requestPagerTest, new Backbone.Paginator.requestPager());
 
-  var server = sinon.fakeServer.create();
-  server.autoRespond = true;
-  server.respondWith([200, { "Content-Type": "application/json" }, '{ "key": "value" }']);
+    var server = sinon.fakeServer.create();
+    server.autoRespond = true;
+    server.respondWith([200, { "Content-Type": "application/json" }, '{ "key": "value" }']);
 
-  var model = {};
+    var model = {};
 
-  var options = {
-    success: function(response, status, xhr) {
-      expect(response['key']).to.equal('value');
-      expect(status).to.equal('success');
-      expect(xhr).to.have.property('status', 200);
-      done();
-    }
-  };
+    var options = {
+      success: function(response, status, xhr) {
+        expect(response['key']).to.equal('value');
+        expect(status).to.equal('success');
+        expect(xhr).to.have.property('status', 200);
+        done();
+      }
+    };
 
-  // execute
-  requestPagerTest.sync(null, model, options);
+    // execute
+    requestPagerTest.sync(null, model, options);
 
-  server.restore();
+    server.restore();
+  });
 });
-
 
