68ca39e5fdbbe8bc2072b606a623ba3691a9d25b
Leonard Ehrenfried <leonard.ehrenfried@gmail.com>
2013-06-12 11:28:22 +0200

    Add spec for Backbone 1.0.0 error callback

--- test/backbone-1.0.0_test.js
+++ test/backbone-1.0.0_test.js
@@ -4,26 +4,29 @@
 
 // this file contains tests which target behaviour found in backbone 1.0.0
 
+var makePager = function(){
+  var pager = {
+    paginator_ui: {},
+    paginator_core: {
+      type: 'GET',
+      dataType: 'json'
+    }
+  };
+  _.extend(pager, new Backbone.Paginator.requestPager());
+  return pager;
+};
+
 describe("Backbone 1.0.0 specific functionality", function() {
   it('should use "promise-style" `options.success` arguments', function(done){
 
     // 0.9.10 had changed the arguments but 1.0.0 reverted back to what it used to be
     // in 0.9.9 and earlier
 
-    var requestPagerTest = {
-      paginator_ui: {},
-      paginator_core: {
-        type: 'GET',
-        dataType: 'json'
-      }
-    };
-    _.extend(requestPagerTest, new Backbone.Paginator.requestPager());
+    var requestPagerTest = makePager();
 
     var server = sinon.fakeServer.create();
     server.autoRespond = true;
     server.respondWith([200, { "Content-Type": "application/json" }, '{ "key": "value" }']);
-
-    var model = {};
 
     var options = {
       success: function(response, status, xhr) {
@@ -35,7 +38,31 @@ describe("Backbone 1.0.0 specific functionality", function() {
     };
 
     // execute
-    requestPagerTest.sync(null, model, options);
+    requestPagerTest.sync(null, {}, options);
+
+    server.restore();
+  });
+
+  it('should use "promise-style" `options.error` arguments', function(done){
+
+    // 0.9.10 had changed the arguments but 1.0.0 reverted back to what it used to be
+    // in 0.9.9 and earlier
+
+    var requestPagerTest = makePager();
+
+    var server = sinon.fakeServer.create();
+    server.autoRespond = true;
+    server.respondWith([401, { "Content-Type": "application/json" }, '{ "key": "value" }']);
+
+    var options = {
+      error: function(model, xhr, options) {
+        expect(xhr).to.have.property('status', 401);
+        done();
+      }
+    };
+
+    // execute
+    requestPagerTest.sync(null, {}, options);
 
     server.restore();
   });
