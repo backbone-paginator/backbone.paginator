7e80f90e999fff321fcfc86ef8e92148a497b9e6
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2013-11-02 22:50:37 +0800

    Commit test case fixes for last commit

--- test/setsorting.js
+++ test/setsorting.js
@@ -45,7 +45,11 @@ $(document).ready(function () {
 
   test("setSorting", function () {
 
-    var col = new Backbone.PageableCollection([
+    var col = new (Backbone.PageableCollection.extend({
+      comparator: function (l, r) {
+        return 0;
+      }
+    }))([
       {id: 2},
       {id: 1},
       {id: 3}
@@ -56,13 +60,13 @@ $(document).ready(function () {
     col.setSorting("id");
     strictEqual(col.state.sortKey, "id");
     strictEqual(col.state.order, -1);
-    ok(_.isUndefined(col.state.comparator));
+    ok(!col.state.comparator);
     deepEqual(col.toJSON(), [{id: 2}, {id: 1}, {id: 3}]);
 
     col.setSorting("id", 1);
     strictEqual(col.state.sortKey, "id");
     strictEqual(col.state.order, 1);
-    ok(_.isUndefined(col.state.comparator));
+    ok(!col.state.comparator);
     deepEqual(col.toJSON(), [{id: 2}, {id: 1}, {id: 3}]);
 
     col.setSorting("id", -1, {full: false});
@@ -81,31 +85,31 @@ $(document).ready(function () {
     col.setSorting("id", -1, {full: false});
     col.sort();
     deepEqual(col.toJSON(), [{id: 1}, {id: 2}, {id: 3}]);
-    ok(_.isUndefined(col.fullCollection.comparator));
+    ok(!col.fullCollection.comparator);
 
     col.setSorting(null);
-    ok(_.isUndefined(col.comparator));
+    ok(!col.comparator);
 
     col.setSorting("id");
     col.fullCollection.sort();
     deepEqual(col.fullCollection.toJSON(), [{id: 1}, {id: 2}, {id: 3}]);
-    ok(_.isUndefined(col.comparator));
+    ok(!col.comparator);
 
     col.setSorting("id", 1);
     col.fullCollection.sort();
     deepEqual(col.fullCollection.toJSON(), [{id: 3}, {id: 2}, {id: 1}]);
-    ok(_.isUndefined(col.comparator));
+    ok(!col.comparator);
 
     col.setSorting(null);
-    ok(_.isUndefined(col.comparator));
-    ok(_.isUndefined(col.fullCollection.comparator));
+    ok(!col.comparator);
+    ok(!col.fullCollection.comparator);
 
     col.setSorting("id", -1, {full: false, sortValue: function (model, sortKey) {
       return model.get(sortKey) / 3;
     }});
     col.sort();
     deepEqual(col.toJSON(), [{id: 1}, {id: 2}, {id: 3}]);
-    ok(_.isUndefined(col.fullCollection.comparator));
+    ok(!col.fullCollection.comparator);
   });
 
 });
