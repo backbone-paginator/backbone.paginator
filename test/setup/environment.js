55ec3b827f31634409548ab7b95741ab53b5b788
Alexey <lesha.ogonkov@gmail.com>
2020-05-06 01:07:33 +0300

    Lint

--- test/setup/environment.js
+++ test/setup/environment.js
@@ -18,8 +18,8 @@
     };
 
     // We never want to actually call these during tests.
-    sinon.stub(window.history, 'pushState');
-    sinon.stub(window.history, 'replaceState');
+    sinon.stub(window.history, "pushState");
+    sinon.stub(window.history, "replaceState");
   });
 
   QUnit.testDone(function () {
